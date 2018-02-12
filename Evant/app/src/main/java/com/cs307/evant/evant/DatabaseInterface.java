38f34402eac8e4235ba03588072d502bf969f070
mcdona64 <mcdona64@purdue.edu>
2018-02-11 23:07:09 -0500

    added mostly empty interface class

---
+++ Evant/app/src/main/java/com/cs307/evant/evant/DatabaseInterface.java
@@ -0,0 +1,30 @@
+package com.cs307.evant.evant;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DatabaseInterface extends {
+
+	// adds a user to the database
+	public int addUser(){
+		return -1;
+	}
+
+	// add an event
+	public int addEvent(){
+		return -1;
+	}
+
+	// add user to attending events
+	public int addAttendees(){
+		return -1;
+	}
+
+	// change event to already having happened
+	public int EventFinished(String name){
+		return -1;
+	}
+
+
+}
