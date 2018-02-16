88e3362c46f04409b45841498d28f13e825cd71e
AviAgarwal <avi7agarwal@gmail.com>
2018-02-16 11:10:11 -0800

    Login Page

---
+++ Evant/app/src/main/java/com/cs307/evant/evant/loginPage.java
@@ -0,0 +1,19 @@
+package com.cs307.evant.evant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by avi12 on 2/16/2018.
+ */
+
+public class loginPage extends AppCompatActivity {
+
+    /*test*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
