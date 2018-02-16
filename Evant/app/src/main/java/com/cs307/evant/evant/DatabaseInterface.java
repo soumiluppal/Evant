f72dbe0b9a9ffcaea77f8ecd7ec85e916df439a9
mcdona64 <mcdona64@purdue.edu>
2018-02-16 13:32:07 -0500

    continued working on the db client

--- Evant/app/src/main/java/com/cs307/evant/evant/DatabaseInterface.java
+++ Evant/app/src/main/java/com/cs307/evant/evant/DatabaseInterface.java
@@ -3,8 +3,47 @@ package com.cs307.evant.evant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /*
 public class DatabaseInterface extends{
+	// This variable will hold the connection to the data base
+	private Connection con;
+	
+	// Constructor for the Interface
+	public DatabaseInterface() throws ConnectionNotEstablishedException {
+		System.out.println("establishing connection...");
+		try {
+			// This will load the MySQL driver, each DB has its own driver
+			Class.forName("com.mysql.jdbc.Driver");
+			// this will ceate the connection
+			this.con = DriverManager.getConnection("jdbc:mysql://128.211.255.58:3306/humorbot", "sqluser", "sqlus    erpw");
+
+		} catch (SQLException e) {
+			System.out.println("connection failed to be established");
+			System.out.println(e.getMessage());
+			throw new ConnectionNotEstablishedException();
+		} catch (ClassNotFoundException e) {
+			System.out.println("failed to load JDBC drivers");
+			throw new ConnectionNotEstablishedException();
+		}
+		System.out.println("connection established");
+}
+
+	public void closeConnection(){
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("closing invalid connection");
+			}
+		}
+		System.out.println("connection closed");
+	}
+
 
 	// adds a user to the database
 	public int addUser(){
