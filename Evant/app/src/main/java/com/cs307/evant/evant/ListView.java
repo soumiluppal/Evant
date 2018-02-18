1238aa3969fbdc4b4bece8921826589031f7bd0c
Abigail Urnes <aurnes@purdue.edu>
2018-02-18 18:57:40 -0500

    Edit ListView activity

--- Evant/app/src/main/java/com/cs307/evant/evant/ListView.java
+++ Evant/app/src/main/java/com/cs307/evant/evant/ListView.java
@@ -1,7 +1,11 @@
 package com.cs307.evant.evant;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListView extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class ListView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        final Button button = findViewById(R.id.homebutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ListView.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListView.this, NewEventActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
