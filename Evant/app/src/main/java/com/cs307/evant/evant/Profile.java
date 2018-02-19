f208c33396fb625f56db6395be8a9658062c43b4
Soumil Uppal <uppals@purdue.edu>
2018-02-18 22:08:49 -0500

    Fixed Profile page layout and added Create event button

--- Evant/app/src/main/java/com/cs307/evant/evant/Profile.java
+++ Evant/app/src/main/java/com/cs307/evant/evant/Profile.java
@@ -1,7 +1,12 @@
 package com.cs307.evant.evant;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profile.this, NewEventActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
