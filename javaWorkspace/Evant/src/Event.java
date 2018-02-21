194f453e519290171815ddd245ed0281f468f1a5
mcdona64 <mcdona64@purdue.edu>
2018-02-21 18:00:27 -0500

    added files for passing data around

---
+++ javaWorkspace/Evant/src/Event.java
@@ -0,0 +1,25 @@
+import java.sql.Date;
+import java.sql.Time;
+
+
+public class Event {
+	// name of the event
+	public String eventName;
+	// time of the event
+	public Time time;
+	// date of the event
+	public Date date;
+	// rating of the event
+	public int rating;
+	// Description of the event
+	public String discription;
+	// Wither or not the event is private
+	public boolean is_private;
+	// max number of attendees
+	public int max_attendees;
+	public User host;
+	
+	// i think these two will change 
+	public String link_to_picture;
+	public int eventLocation;
+}
---
+++ javaWorkspace/Evant/src/User.java
@@ -0,0 +1,20 @@
+// This class will be used to pass user info around
+public class User {
+	// stores the username of a user
+	public String username;
+	
+	// stores a password of a user
+	public String password;
+	
+	// Stores the displayname of the user
+	public String displayname;
+	
+	// stores the users host rating
+	public int host_rating;
+	
+	// I think these will change later
+	public int location;
+	public int radius;
+	public String link_to_progile_picture;
+	public String link_to_facebook;
+}
