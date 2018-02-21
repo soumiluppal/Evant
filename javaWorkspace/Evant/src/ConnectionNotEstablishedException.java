4543b4e094c6a3ef2124d7901e5f9a0cb1bcfaf6
mcdona64 <mcdona64@purdue.edu>
2018-02-21 17:36:04 -0500

    added workspace to make the database interface

---
+++ javaWorkspace/Evant/src/ConnectionNotEstablishedException.java
@@ -0,0 +1,9 @@
+/* this is an exception that will be thrown whenever a connection to the database fails to be accepted */
+public class ConnectionNotEstablishedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
