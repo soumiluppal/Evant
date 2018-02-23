21be2a07dd8bff28db0d0e6a14d678745e4ad654
Soumil Uppal <uppals@purdue.edu>
2018-02-23 16:03:54 -0500

    Distance to markers fixed

--- Evant/app/src/main/java/com/cs307/evant/evant/MapView.java
+++ Evant/app/src/main/java/com/cs307/evant/evant/MapView.java
@@ -199,9 +199,9 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback, Goo
         mMap.setInfoWindowAdapter(new infoWindowAdapter());
 
     }
-
+    LocationManager manager;
     private void startLocationService() {
-        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         long minTime = 10000;
         float minDistance = 0;
@@ -230,7 +230,7 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback, Goo
     public boolean onMarkerClick(Marker marker) {
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 15));
         marker.showInfoWindow();
-        Toast.makeText(getApplicationContext(), "DISTANCE = " + calculateDistance(marker), Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "DISTANCE = " + calculateDistance(marker.getPosition()), Toast.LENGTH_SHORT).show();
 
 
         return true;
@@ -312,14 +312,18 @@ public class MapView extends FragmentActivity implements OnMapReadyCallback, Goo
         mMap.setOnMarkerClickListener(this);
     }
 
-    private double calculateDistance(Marker marker){
-        Location curLoc = new Location("curLoc");
-        curLoc.setLatitude(curLoc.getLatitude());
-        curLoc.setLongitude(curLoc.getLongitude());
+    private double calculateDistance(LatLng marLoc){
+        Location curLoc = new Location(LocationManager.GPS_PROVIDER);
+        try {
+            curLoc = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+        catch (SecurityException e) {
 
-        Location markerLoc = new Location("markerLoc");
-        markerLoc.setLatitude(marker.getPosition().latitude);
-        markerLoc.setLongitude(marker.getPosition().longitude);
+        }
+
+        Location markerLoc = new Location("");
+        markerLoc.setLatitude(marLoc.latitude);
+        markerLoc.setLongitude(marLoc.longitude);
 
         double distance = curLoc.distanceTo(markerLoc);
         distance = distance / 1000;
