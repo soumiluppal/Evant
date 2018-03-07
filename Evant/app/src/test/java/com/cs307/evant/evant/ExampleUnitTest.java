09fc36de171ac18057c6902da9d2e1aa4919315b
Abigail Urnes <aurnes@purdue.edu>
2018-03-07 03:00:57 -0500

    Test environment setup and add simple unit test

--- Evant/app/src/test/java/com/cs307/evant/evant/ExampleUnitTest.java
+++ Evant/app/src/test/java/com/cs307/evant/evant/ExampleUnitTest.java
@@ -1,17 +1,35 @@
 package com.cs307.evant.evant;
 
 import org.junit.Test;
-
+import org.junit.runner.*;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.regex.Pattern;
 import static org.junit.Assert.*;
+
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(value = Parameterized.class)
 public class ExampleUnitTest {
+    private String email;
+    public ExampleUnitTest(String e){
+        this.email = e;
+    }
+
+    @Parameters
+    public static String[] data(){
+        String[] data = new String[] {"name@example.com", "algkhagh@gmail.com"};
+        return data;
+    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue(){
+
+        assertEquals(true, email.matches(".+@.+\\..+"));
     }
 }
