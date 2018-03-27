41ff7cce03b9e06d7fe3030d1ea80a366c60e7e7
Adam McDonald <mcdona64@gmail.com>
2018-03-27 15:18:06 -0400

    gave functionality to buttons

--- Evant/app/src/main/java/com/cs307/evant/evant/EditProfileSettings.java
+++ Evant/app/src/main/java/com/cs307/evant/evant/EditProfileSettings.java
@@ -1,11 +1,13 @@
 package com.cs307.evant.evant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class EditProfileSettings extends AppCompatActivity {
 
@@ -16,7 +18,30 @@ public class EditProfileSettings extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        
+        //Apply button
+        Button ApplyButton = (Button) findViewById(R.id.Apply);
+
+        ApplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EditProfileSettings.this, Settings.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // Cancel button
+        Button CancelButton = (Button) findViewById(R.id.Cancel);
+
+        CancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EditProfileSettings.this, Settings.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 }
