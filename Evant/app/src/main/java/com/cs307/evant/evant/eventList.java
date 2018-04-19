fa50187ecf01f8e47cddb79db1be2f50099afb3a
Abigail Urnes <aurnes@purdue.edu>
2018-04-18 21:20:06 -0400

    Fix toolbar on eventList

--- Evant/app/src/main/java/com/cs307/evant/evant/eventList.java
+++ Evant/app/src/main/java/com/cs307/evant/evant/eventList.java
@@ -1,5 +1,6 @@
 package com.cs307.evant.evant;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Debug;
@@ -12,6 +13,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 
 import java.lang.reflect.Array;
@@ -42,6 +44,40 @@ public class eventList extends AppCompatActivity {
 
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        Button button = findViewById(R.id.homebutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(eventList.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button profileButton = (Button) findViewById(R.id.profilebutton);
+
+
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(eventList.this, Profile.class);
+                intent.putExtra("uid", db.getUid());
+                startActivity(intent);
+            }
+        });
+
+        //attempt
+        Button settingsButton = (Button) findViewById(R.id.settingsbutton);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(eventList.this, Settings.class);
+                intent.putExtra("uid", db.getUid());
+                startActivity(intent);
+            }
+        });
+
         if(getIntent().getStringExtra("Searching") != null && getIntent().getStringExtra("Searching").equals("true"))
         {
             wrd = getIntent().getStringExtra("Word");
